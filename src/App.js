4f9533f41fbff75c73d86a3fae29085fc302fd31
Jenna Ram <jr13114@my.bristol.ac.uk>
2022-05-17 09:42:14 +0100

    add timer for fetching process as currently is

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import ImageGrid from './components/ImageGrid';
 const App = () => {
   const [images, setImages] = useState();
 
+  // receive response from API and set images state to that data
+  // measure time taken to receive API data and log to console as 'timer1'
   useEffect(() => {
+    console.time('timer1')
     fetch('images?limit=10')
       .then(res => res.json())
       .then(data => {
+        console.timeEnd("timer1");
         console.log('Success:', data);
         setImages(data);
       })
