59e54cc0d6dfb2a06c6fa753e21ae785662f4fe9
Jenna Ram <jr13114@my.bristol.ac.uk>
2022-05-17 09:17:15 +0100

    add columns to grid

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -5,7 +5,7 @@ export default function ImageGrid({images}){
             <div className='row text-center text-lg-start'>
                 {
                     images && images.map(img => (
-                    <div key={img.id} >
+                    <div key={img.id} className="col-lg-3 col-md-4 col-6">
                         <img src={`${img.url}.jpg`} alt=''/>
                         <img src={`${img.user.profile_image}.webp`} alt=''/>
                     </div>
