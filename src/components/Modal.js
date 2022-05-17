e6bf17147484f3a6016035bb3a035ba6e55ba0c9
Jenna Ram <jr13114@my.bristol.ac.uk>
2022-05-17 10:55:24 +0100

    add body to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,26 +1,52 @@
-export default function Modal({show}){
+export default function Modal({show, onClose}){
+
+    if (!show){
+        return null
+    }
 
     return (
         <>
-            <div class="modal" tabindex="-1" role="dialog">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Submit Form</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+            <div className='modal'>
+                <div className='modal-content'>
+                    <div className='modal-header'>
+                        <h4 className='modal-title'>Please complete the form</h4>
+                    </div>
+                    <div className='modal-body'>
+                        <form>
+                            
+                            <div class="form-group">
+                                <label for="fullName">Full Name</label>
+                                <input type="password" class="form-control" id="fullName" placeholder="Please enter your full name" />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="Email">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Please enter your email" />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="DOB">Date of Birth</label>
+                                <input type="datetime" class="form-control" id="dob" aria-describedby="dob" placeholder="Please enter your DOB" />
+                                <small id="dob" class="form-text text-muted">Format DD/MM/YYYY.</small>
+                            </div>
+  
+                            <div class="form-group">
+                                <label for="colour">Favourite Colour</label>
+                                <input type="text" class="form-control" id="colour" aria-describedby="colour" placeholder="e.g green" />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="salary">Salary expectations (range)</label>
+                                <input type="text" class="form-control" id="salary" aria-describedby="salary" placeholder="e.g. 25000-30000" />
+                            </div>
+                            
+                        </form>
+                        </div>
+                        <div className='modal-footer'>
+                            <button type="button" class="btn btn-success" onClick={onClose}>Submit</button>
+                        </div>
+                 </div>
+            </div>
         </>
     )
 }
