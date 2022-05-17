2112e394c60481af5fbea6089c88722f7f1f703e
Jenna Ram <jr13114@my.bristol.ac.uk>
2022-05-17 09:28:43 +0100

    grid styling

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -5,9 +5,20 @@ export default function ImageGrid({images}){
             <div className='row text-center text-lg-start'>
                 {
                     images && images.map(img => (
-                    <div key={img.id} className="col-lg-3 col-md-4 col-6">
-                        <img className='car-image'src={`${img.url}.jpg`} alt={img.alt_description}/>
-                        <img src={`${img.user.profile_image}.webp`} alt=''/>
+                    <div key={img.id} className="col-lg-3 col-md-4 col-6" style={{padding:'20px'}}>
+                        <div className="img-post">
+                            <img className='car-image'src={`${img.url}.jpg`} alt={img.alt_description}/>
+                            <div className="overlay">
+                      <div className='text'>
+                        {`Likes: ${img.likes}`}
+                      </div>
+                    </div>
+                            <div>
+                                Added by {img.user.name}
+                                <br/>
+                                <img className="user-image" src={`${img.user.profile_image}.webp`} alt='avatar of user who uploaded car image'/>
+                            </div>
+                        </div>
                     </div>
                     ))
                 }
